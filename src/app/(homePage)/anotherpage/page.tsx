eb647f68808d1a88fb28213a2c7c80114becb2c4
omarKosa <omar.a.k2004ks@gmail.com>
2025-11-14 02:01:20 +0300

    created a navbar component

---
+++ src/app/(homePage)/anotherpage/page.tsx
@@ -0,0 +1,9 @@
+const Apage = () => {
+    return (
+        <div>
+            this page will have a nav bar because layout.tsx is in the same folder(homePage) which is
+            called a route group. So all pages inside this route group will have the same layout.
+        </div>
+    );
+}
+export default Apage
