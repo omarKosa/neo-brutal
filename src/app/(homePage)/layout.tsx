6179121ae4fdcf17756c6df3c4798801cfde5d12
omarKosa <omar.a.k2004ks@gmail.com>
2025-11-14 23:51:57 +0300

    modification in dev 14/11:51

--- src/app/(homePage)/layout.tsx
+++ src/app/(homePage)/layout.tsx
@@ -5,7 +5,7 @@ const Page = ({children}: Props) => {
     return (
         <div>
             <nav className="bg-red-500 text-white w-full p-2">
-                <p> Navbar</p>
+                <p> Navbar this is a modification to test github contributes </p>
                 </nav>
             {children}
         </div>
